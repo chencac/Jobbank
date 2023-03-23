5029912c3661a622e64d65714f8ceb99e750aba3
chen <zx656084@gmail.com>
2023-03-23 17:51:59 +0800

    wip commit

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,22 @@
+class ResumesController < ApplicationController
+    def index
+    end
+    def new
+        @resumes = Resume.new
+    end
+
+    def create
+      resume = Resume.new(resume_params)
+
+      if resume.save
+        redirect_to 'root_path'
+      else
+        render html: params
+      end
+    end
+
+    private
+  def resume_params
+    params.require(:resume).permit(:name, :email, :tel, :skill, :intro, :city, :education, :experience, :portfolio)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  '/' , to: 'pages#home', as: 'root'
+  get  '/about' , to: 'pages#about'
+  get  '/contact' , to: 'pages#contact'
+  get  '/resumes' , to: 'resumes#index'
+  get  '/resumes/new' , to: 'resumes#new'
+  post '/resumes' , to: 'resumes#create'
 end
