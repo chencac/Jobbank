3fa4ea51b44a717422b6edad0b7d4b610ec5b76b
chen <zx656084@gmail.com>
2023-03-29 11:15:08 +0800

    resume commit

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,11 @@
 class ResumesController < ApplicationController
+  before_action :find_resume, only: [:show, :eidt, :update, :destroy]
     def index
       @resumes = Resume.order(created_at: :desc)
+
+      if params[:keyword].present?
+        @resumes = @resumes.search(params[:keyword])
+      end
     end
     def new
         @resume = Resume.new
@@ -16,11 +21,31 @@ class ResumesController < ApplicationController
       end
     end
     def show
-      @resume =Resume.find(params[:id])
+
+    end
+
+    def eidt
+
+    end
+    def update
+
+      if @resume.update(resume_params)
+        redirect_to resumes_path, notice: "修改成功"
+        else
+          render :eidt
+      end
+    end
+
+    def destroy
+      @resume.destroy
+      redirect_to resumes_path, notice: "刪除成功"
     end
 
     private
   def resume_params
     params.require(:resume).permit(:name, :email, :tel, :skill, :intro, :city, :education, :experience, :portfolio)
+  end
+  def find_resume
+    @resume = Resume.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  get 'tailwind/index'
+  root 'tailwind#index'
   get  '/about' , to: 'pages#about'
   get  '/contact' , to: 'pages#contact'
+
+
   get  '/resumes' , to: 'resumes#index'
   get  '/resumes/new' , to: 'resumes#new'
   post '/resumes' , to: 'resumes#create'
   get  '/resumes/:id' , to: 'resumes#show', as: 'resume'
+  get  '/resumes/:id/eidt' , to: 'resumes#eidt', as: 'eidt_resume'
+  patch '/resumes/:id', to: 'resumes#update'
+  delete '/resumes/:id', to: 'resumes#destroy'
 end
